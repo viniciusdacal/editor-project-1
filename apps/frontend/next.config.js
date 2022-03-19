2cfecd7182721a199e75b33ef962abdb16880b2b
Vinicius Dacal <viniciusldacal@gmail.com>
2022-03-18 21:12:36 -0300

    config: update nextjs and internal packages names

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require('./package.json');
+
+const packages = Object.keys(pkg.dependencies).filter((name) =>
+  name.includes('@mochary')
+);
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNextTranspileModules = require('next-transpile-modules')(
+  [...packages],
+  {
+    resolveSymlinks: true,
+
+    debug: false,
+  }
+);
+
+const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    esmExternals: true,
+    externalDir: true,
+  },
+};
+
+const config = withNextTranspileModules(nextConfig);
+
+module.exports = config;
